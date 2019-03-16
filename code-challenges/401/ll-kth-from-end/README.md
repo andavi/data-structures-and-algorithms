973fd2c36db8cae9c9c2d7f2d6352530afbef05c
Andrew Davis <andrew.bradley.davis@gmail.com>
2019-03-16 05:21:43 -0700

    initial commit

---
+++ code-challenges/401/ll-kth-from-end/README.md
@@ -0,0 +1,13 @@
+
+
+# Kth from end of linked list
+Find the kth value from the end of a linked list.
+
+## Challenge
+Find the kth value from the end of a linked list catching all edge cases.
+
+## Approach & Efficiency
+Space will be O(1) because we won't be building an additional data structure in our approach. Time complexity will be O(n) because we only want to make one traversal of the LL.
+
+## Solution
+![alt text](https://raw.githubusercontent.com/andavi/data-structures-and-algorithms/master/code-challenges/401/ll-kth-from-end/assets/ll-kth-from-end.jpg)'
---
+++ code-challenges/401/ll-kth-from-end/linked-list.js
@@ -0,0 +1,122 @@
+'use strict';
+
+const Node = require('./node');
+
+// Variables
+  // HEAD
+// Behavior
+  // Insert Values -> :)
+  // Remove Values
+  // Iterate / Loop -> :)
+class LinkedList{
+  constructor(){
+    this.head = null;
+  }
+
+  insertAtHead(value){
+    const newNode = new Node(value);
+    // 1 - Connecting newNode to our list
+    newNode.next = this.head;
+
+    // 2 - re-assigning our HEAD
+    this.head = newNode;
+  }
+
+  insertAtTail(value){
+    const newNode = new Node(value);
+
+    if(!this.head) {
+      this.head = newNode;
+      return;
+    }
+    let current = this.head;
+
+    while(current.next) { // Vinicio - WHILE I CAN MOVE CURRENT TO THE RIGHT
+      current = current.next;
+    }
+    // Vinicio - once this while is over. I know I'm at the end
+    // current.next is equal to null right now
+    // this is making the new connection
+    current.next = newNode;
+  }
+
+  // Vinicio - I'm teaching find, mainly to teach you how to loop
+  find(value) {
+    if(!this.head) {
+      throw new Error('__ERROR__ The list is empty');
+    }
+    if(this.head.value === value) {
+      return this.head;
+    }
+
+    // Vinicio - let's loop over the linked list
+    let current = this.head;
+    while(current.next) {
+      //------------------------------------------------------
+      // Vinicio - While I'm looping I want to check if I find the
+      // value I'm looking for
+      if(current.next.value === value) {
+        return current.next;
+      }
+      //------------------------------------------------------
+      current = current.next;
+    }
+    // Vinicio - if I got to this point, I didn't find anything
+    return null;
+  }
+
+  // Andrew - insert a new node before the node with the given value
+  insertBefore(value, newValue) {
+    const newNode = new Node(newValue);
+    // Andrew - edge case if first new is value
+    let current = this.head;
+    if (current.value === value) {
+      newNode.next = current;
+      this.head = newNode;
+    }
+    while(current.next) {
+      if (current.next.value === value) {
+        newNode.next = current.next;
+        current.next = newNode;
+        break;
+      } else {
+        current = current.next;
+      }
+    }
+  }
+
+  // Andrew - insert new node after the node with given value
+  insertAfter(value, newValue) {
+    const newNode = new Node(newValue);
+    let current = this.head;
+    while(current) {
+      if (current.value === value) {
+        newNode.next = current.next;
+        current.next = newNode;
+        break;
+      } else {
+        current = current.next;
+      }
+    }
+  }
+  // Delete first instance of giiven value
+  delete(value) {
+    let current = this.head;
+    // Andrew - edge case first value is it
+    if (current.value === value) {
+      current = current.next;
+      this.head = current;
+    }
+    while(current.next) {
+      if (current.next.value === value) {
+        current.next = current.next.next;
+      } else {
+        current = current.next;
+      }
+    }
+  }
+}
+
+
+
+module.exports = LinkedList;
